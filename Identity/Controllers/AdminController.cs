db8f9f7c5e1e21c14ee67fd3598e3ef717e9cf2c
Sandhia MLeod <mcleodsandhia@gmail.com>
2020-09-01 12:09:27 -0400

    Update

--- Identity/Controllers/AdminController.cs
+++ Identity/Controllers/AdminController.cs
@@ -33,10 +33,12 @@ namespace Identity.Controllers
     public class AdminController : Controller
     {
         private UserManager<AppUser> userManager;
+        private IPasswordHasher<AppUser> passwordHasher;
 
-        public AdminController(UserManager<AppUser> usrMgr)
+        public AdminController(UserManager<AppUser> usrMgr, IPasswordHasher<AppUser> passwordHash)
         {
             userManager = usrMgr;
+            passwordHasher = passwordHash;
         }
 
         public IActionResult Index()   // https://localhost:44327/Admin
@@ -74,6 +76,51 @@ namespace Identity.Controllers
             }
 
             return View(user);
+        }
+
+        public async Task<IActionResult> Update(string id)
+        {
+            AppUser user = await userManager.FindByIdAsync(id);
+            if (user != null)
+                return View(user);
+            else
+                return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(string id, string email, string password)
+        {
+            AppUser user = await userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                if (!string.IsNullOrEmpty(email))
+                    user.Email = email;
+                else
+                    ModelState.AddModelError("", "Email cannot be empty");
+
+                if (!string.IsNullOrEmpty(password))
+                    user.PasswordHash = passwordHasher.HashPassword(user, password);
+                else
+                    ModelState.AddModelError("", "Password cannot be empty");
+
+                if (!string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(password))
+                {
+                    IdentityResult result = await userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                        return RedirectToAction("Index");
+                    else
+                        Errors(result);
+                }
+            }
+            else
+                ModelState.AddModelError("", "User Not Found");
+            return View(user);
+        }
+
+        private void Errors(IdentityResult result)
+        {
+            foreach (IdentityError error in result.Errors)
+                ModelState.AddModelError("", error.Description);
         }
     }
 }
