e22b2a7ab0bd739b00833402164d48b248c23769
Sandhia MLeod <mcleodsandhia@gmail.com>
2020-09-04 11:19:08 -0400

    Update action

--- Identity/Controllers/AdminController.cs
+++ Identity/Controllers/AdminController.cs
@@ -171,6 +171,23 @@ namespace Identity.Controllers
             return View(user);
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            AppUser user = await userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                IdentityResult result = await userManager.DeleteAsync(user);
+                if (result.Succeeded)
+                    return RedirectToAction("Index");
+                else
+                    Errors(result);
+            }
+            else
+                ModelState.AddModelError("", "User Not Found");
+            return View("Index", userManager.Users);
+        }
+
         private void Errors(IdentityResult result)
         {
             foreach (IdentityError error in result.Errors)
